bf5430e62d55ae994c3ed2c5d114bee56bddf9cc
James781004@hotmail.com <James781004@hotmail.com>
2019-05-08 10:11:05 +0800

    about the service

---
+++ src/main/java/com/James/crudDemo/Service/userServiceImpl.java
@@ -0,0 +1,10 @@
+package com.James.crudDemo.Service;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class userServiceImpl {
+
+    // in the basic case, service might not be needed
+    // service should be created if the complicated manipulation is existing
+}
